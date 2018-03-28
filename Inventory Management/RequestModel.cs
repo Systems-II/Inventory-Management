7fc352eda07deacb9a802b48e718a2b0b53b894e
awmoore95 <awmoore95@yahoo.com>
2018-03-28 12:46:55 -0500

    Created class RequestModel.cs to model data for inventory requests.

---
+++ Inventory Management/RequestModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management
+{
+    class RequestModel
+    {
+        string tNumber;
+        string notes;
+        int familySize;
+        DateTime dateRequested;
+        DateTime dateFilled;
+        DateTime datePickedUp;
+
+        public string TNumber { get => tNumber; set => tNumber = value; }
+        public string Notes { get => notes; set => notes = value; }
+        public int FamilySize { get => familySize; set => familySize = value; }
+        public DateTime DateRequested { get => dateRequested; set => dateRequested = value; }
+        public DateTime DateFilled { get => dateFilled; set => dateFilled = value; }
+        public DateTime DatePickedUp { get => datePickedUp; set => datePickedUp = value; }
+    }
+}
