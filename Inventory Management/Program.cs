cd6332842f151e3ffd0c6ba6af9bfb19250b9388
awmoore95 <awmoore95@yahoo.com>
2018-08-23 21:36:49 -0500

    Added the bestest test comment

--- Inventory Management/Program.cs
+++ Inventory Management/Program.cs
@@ -9,6 +9,8 @@ namespace Inventory_Management
     static class Program
     {
 
+        //this is the bestest test comment
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
